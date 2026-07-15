685a2393086693296133cf46735a1c6e5d855a7b
BrianSechelo <sechbrian123.gmail.com>
2026-07-15 16:28:27 +0300

    Implement JWT authentication and role-based access control

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { Roles } from './decorators/roles.decorator';
+import { RolesGuard } from './guards/roles.guard';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 
@@ -16,4 +19,18 @@ export class AuthController {
     login(@Body() loginDto: LoginDto) {
         return this.authService.login(loginDto);
     }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('profile')
+    getProfile(@Req() req){
+        return req.user;
+    }
+    @Roles('ADMIN')
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Get('admin')
+    getAdminData() {
+        return {
+        message: 'Welcome Admin!',
+  };
+}
 }
